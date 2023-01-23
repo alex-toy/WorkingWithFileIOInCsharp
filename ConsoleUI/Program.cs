19c1a25fbabd52b45c29469486bfc5174de8b5e3
alex-toy <alexei.80@hotmail.fr>
2023-01-23 19:22:10 +0100

    init

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\source\CSsharpPackages\WorkingWithFileIOInCsharp\FileSystem";
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
